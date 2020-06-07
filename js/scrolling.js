68e4f385d92d123fc04369ec819b070d0fe8efbd
covacode <covascode@gmail.com>
2020-06-07 16:38:12 -0300

    Updates Curriculum

--- js/scrolling.js
+++ js/scrolling.js
@@ -4,7 +4,27 @@ $('#Home').on("click", function () {
     $('html, body').animate({scrollTop: dest}, 800);
 });
 
+$('#Home_Phone').on("click", function () {
+    var dest = $("#page-top").offset().top;
+    $('html, body').animate({scrollTop: dest}, 800);
+});
+
 $('#About').on("click", function () {
     var dest = $("#about").offset().top;
+    $('html, body').animate({scrollTop: dest}, 800);
+});
+
+$('#About_Phone').on("click", function () {
+    var dest = $("#about").offset().top;
+    $('html, body').animate({scrollTop: dest}, 800);
+});
+
+$('#Curriculum').on("click", function () {
+    var dest = $("#curriculum").offset().top;
+    $('html, body').animate({scrollTop: dest}, 800);
+});
+
+$('#Curriculum_Phone').on("click", function () {
+    var dest = $("#curriculum").offset().top;
     $('html, body').animate({scrollTop: dest}, 800);
 });
